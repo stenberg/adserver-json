ad55e685d2be79aa5701543ebb3cdf57cdc3afed
Joakim Stenberg <stenberg@users.noreply.github.com>
2019-06-25 00:00:04 +0200

    Delete simple.js

--- simple.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("submit").addEventListener("click", function() {
-  var headline = document.getElementById("headline").value;
-  var body = document.getElementById("bodyText").value;
-  var url = document.getElementById("landingPage").value;
-  console.log(headline)
-  console.log(body)
-  console.log(url)
-});
