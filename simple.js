c8edd5e5cbaca996eabb64146149126e620b9c22
Joakim Stenberg <stenberg@users.noreply.github.com>
2019-06-24 23:56:04 +0200

    Create simple.js

---
+++ simple.js
@@ -0,0 +1,8 @@
+document.getElementById("submit").addEventListener("click", function() {
+  var headline = document.getElementById("headline").value;
+  var body = document.getElementById("bodyText").value;
+  var url = document.getElementById("landingPage").value;
+  console.log(headline)
+  console.log(body)
+  console.log(url)
+});
